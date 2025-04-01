4f553faf830bc9aa32d5fe424815b9f8ba100404
Akmal <akmal_nazrul@abyres.net>
2025-04-02 00:03:33 +0800

    Successfully add geolocation to database

--- test_pgsql_connection.py
+++ test_pgsql_connection.py
@@ -1,15 +1,19 @@
 import psycopg2
 from psycopg2 import OperationalError
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def test_postgresql_connection():
     try:
         # Replace with your actual database credentials
         connection = psycopg2.connect(
-            dbname="Scraping",
-            user="postgres",
-            password="1234",
-            host="localhost",
-            port="5432"
+            dbname = os.getenv('DB_NAME'),
+            user = os.getenv('DB_USER'),
+            password = os.getenv('DB_PASSWORD'),
+            host = os.getenv('DB_HOST'),
+            port = os.getenv('DB_PORT')
         )
         
         print("Connection to PostgreSQL successful!")
